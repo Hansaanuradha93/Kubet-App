b2887d72f0b905b35d3678fd0a6b0bf1b5c65c0c
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-11-12 21:16:36 +0530

    Contants and fonts added

---
+++ Kubet App/Contants/Constants.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// MARK: - Fonts
+struct Fonts {
+    static let avenirNext = "Avenir Next"
+}
+
+
+// MARK: - GlobalConstants
+struct GlobalConstants {
+    static let height: CGFloat = 44
+    static let cornerRadius: CGFloat = 3
+    static let borderWidth: CGFloat = 0.5
+    static let borderColor: UIColor = .gray
+}
